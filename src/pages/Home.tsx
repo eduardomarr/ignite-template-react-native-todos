38efb4813dc79d66c1a15ef5e65da04272467b72
Eduardo Marques <dado.nep@gmail.com>
2021-10-14 10:47:25 -0300

    initial/final commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,19 +5,37 @@ import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    };
+
+    setTasks(prevState => [...prevState, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => ({ ...task }));
+
+    const foundedTask = updatedTasks.find(task => task.id === id);
+
+    if (!foundedTask) {
+      return;
+    }
+
+    foundedTask.done = !foundedTask.done;
+    setTasks(updatedTasks);
+
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const updatedTasks = tasks.filter(task => task.id !== id);
+    setTasks(updatedTasks);
   }
 
   return (
@@ -26,10 +44,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
